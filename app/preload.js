920b8cb038e59f9ace3633f511dd01bfb24a01e2
alexfilth186@gmail.com <IWasBOrn1986>
2020-10-19 14:47:04 +0300

    [undoc] Fixed the logging with disabled Electron debugging options

--- app/preload.js
+++ app/preload.js
@@ -1,8 +1,13 @@
+// access to Node console
+const nodeConsole = require('console');
+const devConsole = new nodeConsole.Console(process.stdout, process.stderr);
+
 window.addEventListener('DOMContentLoaded', () => {
     const configLink = document.getElementById("createConfigLink");
     if (configLink != null) {
         document.getElementById("createConfigLink").addEventListener("click", () => {
             console.log("Clicked on Cat configuration creation link!");
+            devConsole.log("Clicked on Cat configuration creation link!");
         });
     }
 
@@ -10,6 +15,7 @@ window.addEventListener('DOMContentLoaded', () => {
     if (floodLog != null) {
         document.querySelector(".floodLog").addEventListener('input', () => {
             console.log(floodLog.value);
+            devConsole.log(floodLog.value);
         });
     }
 });
